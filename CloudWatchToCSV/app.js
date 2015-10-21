4b4371871d0a23aa8687e9f0715c0b384b950aeb
Štěpán Bechynský <stepan@bechynsky.cz>
2015-10-21 09:00:13 +0200

    init

---
+++ CloudWatchToCSV/app.js
@@ -0,0 +1,26 @@
+﻿var AWS = require('aws-sdk');
+AWS.config.region = 'eu-west-1';
+
+var cloudwatch = new AWS.CloudWatch();
+
+var endTime = new Date();
+var startTime = new Date();
+startTime.setHours(endTime.getHours() - 1);
+
+var params = {
+	StartTime: startTime,
+	EndTime: endTime,
+	Period: 300,
+	Namespace: 'AWS/EC2',
+	MetricName: 'CPUUtilization',
+	Statistics: ['SampleCount', 'Average', 'Sum', 'Minimum', 'Maximum']
+};
+
+cloudwatch.getMetricStatistics(params, function (error, data) {
+	if (error) {
+		console.log(error.message);
+	}
+	else {
+		console.log(data);
+	}
+});
